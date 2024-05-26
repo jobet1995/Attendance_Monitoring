ecba23b617d8ef91371acba1e7933a06c8f4ba2f
Jobet Casquejo <jobetcasquejo05@gmail.com>
2024-05-26 03:45:38 +0000

    style: Update quotes to use double quotes instead of single quotes in multiple files

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,197 +1,249 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import (
-    User, Product, Supplier, ProductSupplier, Warehouse, Inventory,
-    Order, OrderDetail, Customer, CustomerOrder, CustomerOrderDetail,
-    Shipment, ShipmentDetail, StockAdjustment, InventoryTransaction
+    User,
+    Product,
+    Supplier,
+    ProductSupplier,
+    Warehouse,
+    Inventory,
+    Order,
+    OrderDetail,
+    Customer,
+    CustomerOrder,
+    CustomerOrderDetail,
+    Shipment,
+    ShipmentDetail,
+    StockAdjustment,
+    InventoryTransaction,
 )
 
 
 class UserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2',
-                  'email', 'first_name', 'last_name', 'role']
+        fields = [
+            "username",
+            "password1",
+            "password2",
+            "email",
+            "first_name",
+            "last_name",
+            "role",
+        ]
         widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-control'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control'}),
-            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'role': forms.Select(attrs={'class': 'form-control'}),
+            "username": forms.TextInput(attrs={"class": "form-control"}),
+            "email": forms.EmailInput(attrs={"class": "form-control"}),
+            "first_name": forms.TextInput(attrs={"class": "form-control"}),
+            "last_name": forms.TextInput(attrs={"class": "form-control"}),
+            "role": forms.Select(attrs={"class": "form-control"}),
         }
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['product_name', 'description',
-                  'category', 'unit_price', 'reorder_level']
+        fields = [
+            "product_name",
+            "description",
+            "category",
+            "unit_price",
+            "reorder_level",
+        ]
         widgets = {
-            'product_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'description': forms.Textarea(attrs={'class': 'form-control'}),
-            'category': forms.TextInput(attrs={'class': 'form-control'}),
-            'unit_price': forms.NumberInput(attrs={'class': 'form-control'}),
-            'reorder_level': forms.NumberInput(attrs={'class': 'form-control'}),
+            "product_name": forms.TextInput(attrs={"class": "form-control"}),
+            "description": forms.Textarea(attrs={"class": "form-control"}),
+            "category": forms.TextInput(attrs={"class": "form-control"}),
+            "unit_price": forms.NumberInput(attrs={"class": "form-control"}),
+            "reorder_level": forms.NumberInput(attrs={"class": "form-control"}),
         }
 
 
 class SupplierForm(forms.ModelForm):
     class Meta:
         model = Supplier
-        fields = ['supplier_name', 'contact_name', 'address',
-                  'city', 'postal_code', 'country', 'phone']
+        fields = [
+            "supplier_name",
+            "contact_name",
+            "address",
+            "city",
+            "postal_code",
+            "country",
+            "phone",
+        ]
         widgets = {
-            'supplier_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'contact_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'address': forms.Textarea(attrs={'class': 'form-control'}),
-            'city': forms.TextInput(attrs={'class': 'form-control'}),
-            'postal_code': forms.TextInput(attrs={'class': 'form-control'}),
-            'country': forms.TextInput(attrs={'class': 'form-control'}),
-            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            "supplier_name": forms.TextInput(attrs={"class": "form-control"}),
+            "contact_name": forms.TextInput(attrs={"class": "form-control"}),
+            "address": forms.Textarea(attrs={"class": "form-control"}),
+            "city": forms.TextInput(attrs={"class": "form-control"}),
+            "postal_code": forms.TextInput(attrs={"class": "form-control"}),
+            "country": forms.TextInput(attrs={"class": "form-control"}),
+            "phone": forms.TextInput(attrs={"class": "form-control"}),
         }
 
 
 class ProductSupplierForm(forms.ModelForm):
     class Meta:
         model = ProductSupplier
-        fields = ['product', 'supplier']
+        fields = ["product", "supplier"]
         widgets = {
-            'product': forms.Select(attrs={'class': 'form-control'}),
-            'supplier': forms.Select(attrs={'class': 'form-control'}),
+            "product": forms.Select(attrs={"class": "form-control"}),
+            "supplier": forms.Select(attrs={"class": "form-control"}),
         }
 
 
 class WarehouseForm(forms.ModelForm):
     class Meta:
         model = Warehouse
-        fields = ['warehouse_name', 'location']
+        fields = ["warehouse_name", "location"]
         widgets = {
-            'warehouse_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'location': forms.TextInput(attrs={'class': 'form-control'}),
+            "warehouse_name": forms.TextInput(attrs={"class": "form-control"}),
+            "location": forms.TextInput(attrs={"class": "form-control"}),
         }
 
 
 class InventoryForm(forms.ModelForm):
     class Meta:
         model = Inventory
-        fields = ['product', 'warehouse', 'quantity']
+        fields = ["product", "warehouse", "quantity"]
         widgets = {
-            'product': forms.Select(attrs={'class': 'form-control'}),
-            'warehouse': forms.Select(attrs={'class': 'form-control'}),
-            'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
+            "product": forms.Select(attrs={"class": "form-control"}),
+            "warehouse": forms.Select(attrs={"class": "form-control"}),
+            "quantity": forms.NumberInput(attrs={"class": "form-control"}),
         }
 
 
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ['order_date', 'supplier', 'status']
+        fields = ["order_date", "supplier", "status"]
         widgets = {
-            'order_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
-            'supplier': forms.Select(attrs={'class': 'form-control'}),
-            'status': forms.Select(attrs={'class': 'form-control'}),
+            "order_date": forms.DateInput(
+                attrs={"class": "form-control", "type": "date"}
+            ),
+            "supplier": forms.Select(attrs={"class": "form-control"}),
+            "status": forms.Select(attrs={"class": "form-control"}),
         }
 
 
 class OrderDetailForm(forms.ModelForm):
     class Meta:
         model = OrderDetail
-        fields = ['order', 'product', 'quantity', 'unit_price']
+        fields = ["order", "product", "quantity", "unit_price"]
         widgets = {
-            'order': forms.Select(attrs={'class': 'form-control'}),
-            'product': forms.Select(attrs={'class': 'form-control'}),
-            'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
-            'unit_price': forms.NumberInput(attrs={'class': 'form-control'}),
+            "order": forms.Select(attrs={"class": "form-control"}),
+            "product": forms.Select(attrs={"class": "form-control"}),
+            "quantity": forms.NumberInput(attrs={"class": "form-control"}),
+            "unit_price": forms.NumberInput(attrs={"class": "form-control"}),
         }
 
 
 class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
-        fields = ['customer_name', 'contact_name', 'address',
-                  'city', 'postal_code', 'country', 'phone']
+        fields = [
+            "customer_name",
+            "contact_name",
+            "address",
+            "city",
+            "postal_code",
+            "country",
+            "phone",
+        ]
         widgets = {
-            'customer_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'contact_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'address': forms.Textarea(attrs={'class': 'form-control'}),
-            'city': forms.TextInput(attrs={'class': 'form-control'}),
-            'postal_code': forms.TextInput(attrs={'class': 'form-control'}),
-            'country': forms.TextInput(attrs={'class': 'form-control'}),
-            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            "customer_name": forms.TextInput(attrs={"class": "form-control"}),
+            "contact_name": forms.TextInput(attrs={"class": "form-control"}),
+            "address": forms.Textarea(attrs={"class": "form-control"}),
+            "city": forms.TextInput(attrs={"class": "form-control"}),
+            "postal_code": forms.TextInput(attrs={"class": "form-control"}),
+            "country": forms.TextInput(attrs={"class": "form-control"}),
+            "phone": forms.TextInput(attrs={"class": "form-control"}),
         }
 
 
 class CustomerOrderForm(forms.ModelForm):
     class Meta:
         model = CustomerOrder
-        fields = ['customer', 'order_date', 'status']
+        fields = ["customer", "order_date", "status"]
         widgets = {
-            'customer': forms.Select(attrs={'class': 'form-control'}),
-            'order_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
-            'status': forms.Select(attrs={'class': 'form-control'}),
+            "customer": forms.Select(attrs={"class": "form-control"}),
+            "order_date": forms.DateInput(
+                attrs={"class": "form-control", "type": "date"}
+            ),
+            "status": forms.Select(attrs={"class": "form-control"}),
         }
 
 
 class CustomerOrderDetailForm(forms.ModelForm):
     class Meta:
         model = CustomerOrderDetail
-        fields = ['customer_order', 'product', 'quantity', 'unit_price']
+        fields = ["customer_order", "product", "quantity", "unit_price"]
         widgets = {
-            'customer_order': forms.Select(attrs={'class': 'form-control'}),
-            'product': forms.Select(attrs={'class': 'form-control'}),
-            'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
-            'unit_price': forms.NumberInput(attrs={'class': 'form-control'}),
+            "customer_order": forms.Select(attrs={"class": "form-control"}),
+            "product": forms.Select(attrs={"class": "form-control"}),
+            "quantity": forms.NumberInput(attrs={"class": "form-control"}),
+            "unit_price": forms.NumberInput(attrs={"class": "form-control"}),
         }
 
 
 class ShipmentForm(forms.ModelForm):
     class Meta:
         model = Shipment
-        fields = ['shipment_date', 'carrier', 'tracking_number', 'status']
+        fields = ["shipment_date", "carrier", "tracking_number", "status"]
         widgets = {
-            'shipment_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
-            'carrier': forms.TextInput(attrs={'class': 'form-control'}),
-            'tracking_number': forms.TextInput(attrs={'class': 'form-control'}),
-            'status': forms.Select(attrs={'class': 'form-control'}),
+            "shipment_date": forms.DateInput(
+                attrs={"class": "form-control", "type": "date"}
+            ),
+            "carrier": forms.TextInput(attrs={"class": "form-control"}),
+            "tracking_number": forms.TextInput(attrs={"class": "form-control"}),
+            "status": forms.Select(attrs={"class": "form-control"}),
         }
 
 
 class ShipmentDetailForm(forms.ModelForm):
     class Meta:
         model = ShipmentDetail
-        fields = ['shipment', 'order', 'customer_order', 'product', 'quantity']
+        fields = ["shipment", "order", "customer_order", "product", "quantity"]
         widgets = {
-            'shipment': forms.Select(attrs={'class': 'form-control'}),
-            'order': forms.Select(attrs={'class': 'form-control'}),
-            'customer_order': forms.Select(attrs={'class': 'form-control'}),
-            'product': forms.Select(attrs={'class': 'form-control'}),
-            'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
+            "shipment": forms.Select(attrs={"class": "form-control"}),
+            "order": forms.Select(attrs={"class": "form-control"}),
+            "customer_order": forms.Select(attrs={"class": "form-control"}),
+            "product": forms.Select(attrs={"class": "form-control"}),
+            "quantity": forms.NumberInput(attrs={"class": "form-control"}),
         }
 
 
 class StockAdjustmentForm(forms.ModelForm):
     class Meta:
         model = StockAdjustment
-        fields = ['product', 'warehouse',
-                  'adjustment_date', 'quantity', 'reason']
+        fields = ["product", "warehouse", "adjustment_date", "quantity", "reason"]
         widgets = {
-            'product': forms.Select(attrs={'class': 'form-control'}),
-            'warehouse': forms.Select(attrs={'class': 'form-control'}),
-            'adjustment_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
-            'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
-            'reason': forms.TextInput(attrs={'class': 'form-control'}),
+            "product": forms.Select(attrs={"class": "form-control"}),
+            "warehouse": forms.Select(attrs={"class": "form-control"}),
+            "adjustment_date": forms.DateInput(
+                attrs={"class": "form-control", "type": "date"}
+            ),
+            "quantity": forms.NumberInput(attrs={"class": "form-control"}),
+            "reason": forms.TextInput(attrs={"class": "form-control"}),
         }
 
 
 class InventoryTransactionForm(forms.ModelForm):
     class Meta:
         model = InventoryTransaction
-        fields = ['product', 'warehouse', 'quantity',
-                  'transaction_type', 'transaction_date']
+        fields = [
+            "product",
+            "warehouse",
+            "quantity",
+            "transaction_type",
+            "transaction_date",
+        ]
         widgets = {
-            'product': forms.Select(attrs={'class': 'form-control'}),
-            'warehouse': forms.Select(attrs={'class': 'form-control'}),
-            'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
-            'transaction_type': forms.Select(attrs={'class': 'form-control'}),
-            'transaction_date': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
+            "product": forms.Select(attrs={"class": "form-control"}),
+            "warehouse": forms.Select(attrs={"class": "form-control"}),
+            "quantity": forms.NumberInput(attrs={"class": "form-control"}),
+            "transaction_type": forms.Select(attrs={"class": "form-control"}),
+            "transaction_date": forms.DateTimeInput(
+                attrs={"class": "form-control", "type": "datetime-local"}
+            ),
         }
