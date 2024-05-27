c4909098fe7228a6702d91552e5ee749c34dc435
JC <jobetcasquejo27@hotmail.com>
2024-05-27 14:36:21 +0000

    style: Format code , django_project/urls.py, inventory/forms.py, inventory/migrations/0001_initial.py, inventory/migrations/0002_alter_user_role_event_task.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -83,7 +82,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "django_project.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -94,25 +92,27 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME":
+        "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME":
+        "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME":
+        "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME":
+        "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
@@ -124,7 +124,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
--- inventory/migrations/0002_alter_user_role_event_task.py
+++ inventory/migrations/0002_alter_user_role_event_task.py
@@ -53,7 +53,8 @@ class Migration(migrations.Migration):
                 ("location", models.CharField(max_length=255)),
                 (
                     "participants",
-                    models.ManyToManyField(related_name="events", to="inventory.user"),
+                    models.ManyToManyField(related_name="events",
+                                           to="inventory.user"),
                 ),
             ],
         ),
@@ -76,8 +77,8 @@ class Migration(migrations.Migration):
                 (
                     "assigned_to",
                     models.ForeignKey(
-                        on_delete=django.db.models.deletion.CASCADE, to="inventory.user"
-                    ),
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="inventory.user"),
                 ),
             ],
         ),
